7eacd2e349a245cc67b284a43a190d5d81dbddf3
Marie Sonko <mariesonko62@gmail.com>
2018-04-20 03:06:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 def dictionary
   dictionary= {}
-  dictionary=  {"hello"=> "hi", "to, two, too" =>"2",
-              "for, four" =>"4", "be" => "b", "you" => "u",
+  dictionary=  {"hello"=> "hi", "to"=>"2", "two" =>"2", "too" =>"2",
+              "for" => "4", "four" =>"4", "be" => "b", "you" => "u",
               "at" => "@", "and" => "&"}
 end
 def word_substituter(tweet)
