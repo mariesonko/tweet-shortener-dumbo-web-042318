afe5d549fcfb85bfe4e0836ff0871c8a7c79c1cc
Marie Sonko <mariesonko62@gmail.com>
2018-04-20 03:04:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,6 +5,7 @@ def dictionary
               "at" => "@", "and" => "&"}
 end
 def word_substituter(tweet)
-    tweet.split.collect {|word| dictionary.keys.include?(word.downcase) ? word = dictionary[word.downcase] : word }.join(" ")
-end
+    tweet.split.collect {|word| dictionary.keys.include?(word.downcase) ?
+       word = dictionary[word.downcase] : word }.join(" ")
+
 end
