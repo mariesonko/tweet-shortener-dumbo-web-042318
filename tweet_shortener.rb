6748111c8cb9295527aa035cb6bc92f8774817ed
Marie Sonko <mariesonko62@gmail.com>
2018-04-20 02:48:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def dictionary
+  dictionary= {}
+  dictionary=  {"hello"=> "hi", "to, two, too" =>"2",
+              "for, four" =>"4", "be" => "b", "you" => "u",
+              "at" => "@", "and" => "&"}
+end
+def word_substituter(tweet)
+  tweet_array = []
+  tweet_array= tweet.split.map do |word|
+    if dictionary.keys.include?(word)
+      new_word = dictionary [word]
+    else word
+  end
+end
+  puts tweet.array.join (" ")
+end
